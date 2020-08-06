613ba55f814583a55efb669f8227dbd3f9f18aee
FrankR-7 <franklinrosales7@gmail.com>
2020-08-05 18:23:38 -0600

    Recursive function part 1: Breaking the laws of physics

--- Level-gen/level-gen.py
+++ Level-gen/level-gen.py
@@ -14,11 +14,33 @@ map = map.T
 map[0] = map[-1] = np.ones(size[0])
 map = map.T
 
+# print(map)
+
+# Recursive function to generate room boundaries
+rooms = []
+room = Room(0, 0, size[1], size[0])
+rooms.append(room)
+
+
+# Faulty
+def generate_rooms(act, level):
+    next = act.split(level)
+    if next != -1:
+        level = next[0]
+        room = Room(*next[1:])
+        rooms.append(room)
+        if not room.state:
+            level = generate_rooms(room, level)
+    if not act.state:
+        level = generate_rooms(act, level)
+    return level
+
+
+map = generate_rooms(room, map)
+
+# Debugging and stuff, I dont like those negative rooms
 print(map)
-
-# just testing the first parts of the algo
-room = Room(0,0,size[1],size[0], first=True)
-map = room.split(map)
-
-print(map)
-
+print('no. of rooms: ', len(rooms))
+debug = list(zip([(room.width, room.height) for room in rooms], [(room.x1, room.x2) for room in rooms],
+                 [(room.y1, room.y2) for room in rooms], [room.state for room in rooms]))
+print(debug)
--- Level-gen/room.py
+++ Level-gen/room.py
@@ -1,24 +1,32 @@
 import random
 
 class Room:
-    def __init__(self, x_1, y_1, x_2, y_2, neighbors=None, first=False):
-        self.start = (x_1, y_1)
-        self.end = (x_2, y_2)
-        if first:
-            self.neighbors = None
-        else:
-            self.neighbors = neighbors
-        self.width = (x_2 - x_1) - 2
-        self.height = (y_2 - y_1) - 2
-        self.size = self.width * self.height
+    def __init__(self, x_1, y_1, x_2, y_2):
         self.x1, self.x2, self.y1, self.y2 = x_1, x_2, y_1, y_2
+        self.start = (self.x1, self.y1)
+        self.end = (self.x2, self.y2)
+        self.width = (self.x2 - self.x1) - 2
+        self.height = (self.y2 - self.y1) - 2
+        self.size = self.width * self.height
+        self.state = False
+        if self.size <= 9:
+            self.state = True
+        elif (self.width in range(0, 7) and self.width > self.height) or (self.width in range(0, 7) and self.height > self.width):
+            self.state = True
+        elif (self.width <= 5) and (self.height <= 5):
+            self.state = True
+        elif (self.width < 0) or (self.height < 0) or (any([self.x1, self.x2, self.y1, self.y2]) < 0):
+            self.state = True
 
     def _draw(self, axis, pos, matrix):
+        pos = pos-1
+        start = self.x1
         if axis == 'y':
             matrix = matrix.T
+            start = self.y1
 
-        for n, o in enumerate(matrix[pos]):
-            if n == 0 or n == len(matrix[pos]):
+        for n, o in enumerate(matrix[pos, start:]):
+            if n == 0 or n == len(matrix[pos, start:]):
                 pass
             else:
                 if o == 1:
@@ -32,21 +40,66 @@ class Room:
         return matrix
 
     def split(self, matrix):
-        # determine which direction the split will be
-        _axis = 'x'
-        if self.width > self.height:
-            _axis = 'y'
-        elif self.height > self.width:
-            _axis = 'x'
-        elif self.width == self.height:
-            _axis = random.choice(('x', 'y'))
+        if self.state:
+            return -1
+        if self.size <= 9:
+            self.state = True
+            return -1
+        elif ((self.width in range(0, 7) and self.width > self.height) or (self.width in range(0, 7) and self.height > self.width)):
+            self.state = True
+            return -1
+        elif (self.width <= 5) and (self.height <= 5):
+            self.state = True
+            return -1
+        elif (self.width < 0) or (self.height < 0) or (any([self.x1, self.x2, self.y1, self.y2]) < 0):
+            self.state = True
+            return -1
+        else:
+            preMatrix = matrix.copy()
+            # determine which direction the split will be
+            axis = 'x'
+            if self.width > self.height:
+                axis = 'y'
+            elif self.height > self.width:
+                axis = 'x'
+            elif self.width == self.height:
+                axis = random.choice(('x', 'y'))
 
-        # determine where the split will be
-        if _axis == 'x':
-            _pt = random.randint(self.x1 + 3, self.x2 - 3)
-        elif _axis == 'y':
-            _pt = random.randint(self.y1 + 3, self.y2 - 3)
+            # determine where the split will be
+            if axis == 'x':
+                try:
+                    pt = random.randint(self.x1 + 3, self.x2 - 3)
+                except:
+                    self.state = True
+                    return -1
+            elif axis == 'y':
+                try:
+                    pt = random.randint(self.y1 + 3, self.y2 - 3)
+                except:
+                    self.state = True
+                    return -1
 
-        matrix = self._draw(_axis, _pt, matrix.copy())
+            matrix = self._draw(axis, pt, matrix.copy())
 
-        return matrix
+            if axis == 'x':
+                self.y2 = self.y2 - pt
+            elif axis == 'y':
+                self.x2 = self.x2 - pt
+
+            if self.size <= 9:
+                self.state = True
+            elif (self.width == 5 and self.width > self.height) or (self.height == 5 and self.height > self.width):
+                self.state = True
+
+            if (self.width < 0) or (self.height < 0) or (any([self.x1, self.x2, self.y1, self.y2]) < 0):
+                self.state = True
+                return -1
+            else:
+                if axis == 'x':
+                    nx, ny = self.x1, self.y2
+                    ox, oy = self.x1, self.y2 + pt
+                elif axis == 'y':
+                    nx, ny = self.x2, self.y1
+                    ox, oy = self.x2 + pt, self.y2
+
+            return matrix, nx, ny, ox, oy
