14de3fd0f5f2b794cbd112403b3ed097dea16168
FrankR-7 <franklinrosales7@gmail.com>
2020-08-06 15:09:08 -0600

    Recursive function part 2: Upgrades boi

--- Level-gen/level-gen.py
+++ Level-gen/level-gen.py
@@ -3,6 +3,7 @@ import sys
 import numpy as np
 import json
 from room import Room
+import matplotlib.pyplot as plt
 
 # set size of the map from cmd args
 size = (int(sys.argv[2]), int(sys.argv[1]))
@@ -13,25 +14,30 @@ map[0] = map[-1] = np.ones(size[1])
 map = map.T
 map[0] = map[-1] = np.ones(size[0])
 map = map.T
+map[0,0] = 2
 
-# print(map)
+size = [unit - 1 for unit in size]
 
 # Recursive function to generate room boundaries
 rooms = []
 room = Room(0, 0, size[1], size[0])
 rooms.append(room)
 
+levels = []
 
 # Faulty
 def generate_rooms(act, level):
     next = act.split(level)
+    # print(next if type(next)==int else '')
+    print(act.size)
     if next != -1:
         level = next[0]
+        levels.append(level)
         room = Room(*next[1:])
         rooms.append(room)
         if not room.state:
             level = generate_rooms(room, level)
-    if not act.state:
+    if not(act.state):
         level = generate_rooms(act, level)
     return level
 
@@ -39,8 +45,13 @@ def generate_rooms(act, level):
 map = generate_rooms(room, map)
 
 # Debugging and stuff, I dont like those negative rooms
-print(map)
+plt.xticks([])
+plt.yticks([])
+plt.imshow(map, cmap='Greys')
+# plt.scatter([room.x1 for room in rooms] + [room.x2 for room in rooms], [room.y1 for room in rooms] + [room.y2 for room in rooms])
+plt.show()
 print('no. of rooms: ', len(rooms))
-debug = list(zip([(room.width, room.height) for room in rooms], [(room.x1, room.x2) for room in rooms],
-                 [(room.y1, room.y2) for room in rooms], [room.state for room in rooms]))
-print(debug)
+# debug = list(zip([(room.width, room.height) for room in rooms], [room.start for room in rooms],
+                 #[room.end for room in rooms], [room.size for room in rooms]))
+# print(debug)
+# print(levels)
--- Level-gen/room.py
+++ Level-gen/room.py
@@ -5,57 +5,63 @@ class Room:
         self.x1, self.x2, self.y1, self.y2 = x_1, x_2, y_1, y_2
         self.start = (self.x1, self.y1)
         self.end = (self.x2, self.y2)
-        self.width = (self.x2 - self.x1) - 2
-        self.height = (self.y2 - self.y1) - 2
+        self.width = (self.x2 - self.x1)
+        self.height = (self.y2 - self.y1)
         self.size = self.width * self.height
         self.state = False
-        if self.size <= 9:
+        if self.size <= 6:
             self.state = True
-        elif (self.width in range(0, 7) and self.width > self.height) or (self.width in range(0, 7) and self.height > self.width):
+            print('1.1')
+        elif (self.width < 4 and self.width > self.height) or (self.height < 4 and self.height > self.width):
             self.state = True
-        elif (self.width <= 5) and (self.height <= 5):
+            print('2.1')
+        elif (self.width < 4) and (self.height < 4):
             self.state = True
-        elif (self.width < 0) or (self.height < 0) or (any([self.x1, self.x2, self.y1, self.y2]) < 0):
+            print('3.1')
+        elif (self.width <= 1) or (self.height <= 1):
+            print('6.1')
+        elif (self.x1 == self.x2) or (self.y1 == self.y2):
             self.state = True
+            print('5.1')
 
-    def _draw(self, axis, pos, matrix):
-        pos = pos-1
-        start = self.x1
-        if axis == 'y':
-            matrix = matrix.T
-            start = self.y1
+    def _draw(self, matrix):
+        wallx, wally = [], []
+        for i in range(self.x1, self.x2):
+            wallx.append(i)
+        for i in range(self.y1, self.y2):
+            wally.append(i)
 
-        for n, o in enumerate(matrix[pos, start:]):
-            if n == 0 or n == len(matrix[pos, start:]):
-                pass
-            else:
-                if o == 1:
-                    break
-                else:
-                    matrix[pos, n] = 1
-
-        if axis == 'y':
-            matrix = matrix.T
+        for m in wallx:
+            for n in [self.y1, self.y2]:
+                matrix[n, m] = 1
+        for m in wally:
+            for n in [self.x1, self.x2]:
+                matrix[m, n] = 1
 
         return matrix
 
     def split(self, matrix):
+        matrix = self._draw(matrix.copy())
         if self.state:
+            print('0')
             return -1
-        if self.size <= 9:
+        elif self.size <= 6:
             self.state = True
+            print('1.2')
             return -1
-        elif ((self.width in range(0, 7) and self.width > self.height) or (self.width in range(0, 7) and self.height > self.width)):
+        elif ((self.width < 4 and self.width > self.height) or (self.height < 4 and self.height > self.width)):
             self.state = True
+            print('2.2')
             return -1
-        elif (self.width <= 5) and (self.height <= 5):
+        elif (self.width < 4) and (self.height < 4):
             self.state = True
+            print('3.2')
             return -1
-        elif (self.width < 0) or (self.height < 0) or (any([self.x1, self.x2, self.y1, self.y2]) < 0):
+        elif (self.width <= 1) or (self.height <= 1):
             self.state = True
+            print('6.2')
             return -1
         else:
-            preMatrix = matrix.copy()
             # determine which direction the split will be
             axis = 'x'
             if self.width > self.height:
@@ -68,38 +74,42 @@ class Room:
             # determine where the split will be
             if axis == 'x':
                 try:
-                    pt = random.randint(self.x1 + 3, self.x2 - 3)
+                    pt = random.randint(self.y1 + 3, self.y2 - 3)
                 except:
+                    print('8.1')
                     self.state = True
                     return -1
             elif axis == 'y':
                 try:
-                    pt = random.randint(self.y1 + 3, self.y2 - 3)
+                    pt = random.randint(self.x1 + 3, self.x2 - 3)
                 except:
+                    print('8.2')
                     self.state = True
                     return -1
 
-            matrix = self._draw(axis, pt, matrix.copy())
+            matrix = self._draw(matrix.copy())
 
             if axis == 'x':
-                self.y2 = self.y2 - pt
+                oy = self.y2
+                self.y2 = (self.y2 - pt) + self.y1
             elif axis == 'y':
-                self.x2 = self.x2 - pt
+                ox = self.x2
+                self.x2 = (self.x2 - pt) + self.x1
 
-            if self.size <= 9:
-                self.state = True
-            elif (self.width == 5 and self.width > self.height) or (self.height == 5 and self.height > self.width):
-                self.state = True
+            self.width = (self.x2 - self.x1) - 2
+            self.height = (self.y2 - self.y1) - 2
+            self.size = self.width * self.height
 
-            if (self.width < 0) or (self.height < 0) or (any([self.x1, self.x2, self.y1, self.y2]) < 0):
+            '''if (self.width <= 1) or (self.height <= 1):
                 self.state = True
+                print('6.3')
                 return -1
-            else:
-                if axis == 'x':
-                    nx, ny = self.x1, self.y2
-                    ox, oy = self.x1, self.y2 + pt
-                elif axis == 'y':
-                    nx, ny = self.x2, self.y1
-                    ox, oy = self.x2 + pt, self.y2
+            else:'''
+            if axis == 'x':
+                nx, ny = self.x1, self.y2
+                ox = self.x1
+            elif axis == 'y':
+                nx, ny = self.x2, self.y1
+                oy =  self.y2
 
             return matrix, nx, ny, ox, oy
