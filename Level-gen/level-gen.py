a773bdc4ea917e510ea8454d0ce52a71c182a155
FrankR-7 <franklinrosales7@gmail.com>
2020-08-10 11:59:55 -0600

    Rework of the whole generator pipeline
    -Added room selection (still wip)
    -Added grass generation
    -Better performance

--- Level-gen/level-gen.py
+++ Level-gen/level-gen.py
@@ -2,6 +2,7 @@
 import sys
 import numpy as np
 import json
+import random
 from room import Room
 import matplotlib.pyplot as plt
 
@@ -10,11 +11,6 @@ size = (int(sys.argv[2]), int(sys.argv[1]))
 
 # create empty map and add bounding bprders
 map = np.zeros(size, dtype='int')
-map[0] = map[-1] = np.ones(size[1])
-map = map.T
-map[0] = map[-1] = np.ones(size[0])
-map = map.T
-map[0,0] = 2
 
 size = [unit - 1 for unit in size]
 
@@ -23,31 +19,31 @@ rooms = []
 room = Room(0, 0, size[1], size[0])
 rooms.append(room)
 
-def generate_rooms(act, level):
-    next = act.split(level)
+def generate_rooms(act):
+    next = act.split()
     if next != -1:
-        level = next[0]
-        room = Room(*next[1:])
+        room = Room(*next)
         rooms.append(room)
         if not room.state:
-            level = generate_rooms(room, level)
+            generate_rooms(room)
     if not(act.state):
-        level = generate_rooms(act, level)
-    return level
+        generate_rooms(act)
+    return None
 
-map = generate_rooms(room, map)
+generate_rooms(room)
 
-# Don't worry about this v
-'''
-for room in rooms:
-    map = room.generate_floor(map)
-'''
+# Room selection
+normal_rooms = random.choices(rooms, k=25)
+
+# Map generation (wip)
+for room in rooms[::2]:
+    map = room.draw(map.copy())
 
 # Debugging and stuff
 plt.xticks([])
 plt.yticks([])
 plt.imshow(map)
 plt.show()
-print('no. of rooms: ', len(rooms))
 
-print(json.dumps(map.tolist()))
+j = {'map': map.tolist()}
+print(json.dumps(j))
--- Level-gen/room.py
+++ Level-gen/room.py
@@ -3,11 +3,10 @@ import random
 class Room:
     def __init__(self, x_1, y_1, x_2, y_2):
         self.x1, self.x2, self.y1, self.y2 = x_1, x_2, y_1, y_2
-        self.start = (self.x1, self.y1)
-        self.end = (self.x2, self.y2)
         self.width = (self.x2 - self.x1)+1
         self.height = (self.y2 - self.y1)+1
         self.size = self.width * self.height
+        self.type = 'normal'
         self.state = False
         if self.size < 9:
             self.state = True
@@ -16,9 +15,11 @@ class Room:
         elif (self.x1 == self.x2) or (self.y1 == self.y2):
             self.state = True
 
-    def _draw(self, matrix):
-        wallx = [i for i in range(self.x1, self.x2)]
-        wally = [i for i in range(self.y1, self.y2)]
+    def draw_walls(self, matrix):
+        # This works, but I think I can optimise this better
+        # If performance ever becomes an issue, I'll change this part
+        wallx = [i for i in range(self.x1, self.x2+1)]
+        wally = [i for i in range(self.y1, self.y2+1)]
 
         for m in wallx:
             for n in [self.y1, self.y2]:
@@ -29,7 +30,8 @@ class Room:
 
         return matrix
 
-    def split(self, matrix):
+    def split(self):
+        # Check for any end cases
         if self.state:
             return -1
         elif self.size < 9:
@@ -62,19 +64,24 @@ class Room:
                     self.state = True
                     return -1
 
+            # Update variables
             if axis == 'x':
                 oy = self.y2
-                self.y2 = pt # (self.y2 - pt) + self.y1
+                self.y2 = pt
             elif axis == 'y':
                 ox = self.x2
-                self.x2 = pt # (self.x2 - pt) + self.x1
+                self.x2 = pt
 
             self.width = (self.x2 - self.x1)
             self.height = (self.y2 - self.y1)
             self.size = self.width * self.height
 
-            matrix = self._draw(matrix.copy())
+            #I'll decide later if I wanna implemet this
+            '''if self.size < 9:
+                self.state = True
+                return -1'''
 
+            # Prepare variables for the child room
             if axis == 'x':
                 nx, ny = self.x1, self.y2
                 ox = self.x2
@@ -82,16 +89,21 @@ class Room:
                 nx, ny = self.x2, self.y1
                 oy = self.y2
 
-            return matrix, nx, ny, ox, oy
+            return nx, ny, ox, oy
 
-    def generate_floor(self, matrix):
-        sx1, sx2, sy1, sy2, floor = self.x1+1, self.x2-1, self.y1+1, self.y2-1, []
-        for x in range(sx1, sx2):
-            for y in range(sy1, sx2):
-                #floor.append((x, y))
-                matrix[y, x] = 2
-
-        #for n, m in floor:
-            #matrix[n, m] = 2
+    def draw(self, matrix):
+        # Unified func for updating the map, will give better performance
+        if self.type in ['hall','void_hall']:
+            pass # Coming soon
+        elif self.type in ['normal','chest','start','end']:
+            matrix = self.draw_walls(matrix.copy())
+            floorx = [i for i in range(self.x1+1, self.x2)]
+            floory = [i for i in range(self.y1+1, self.y2)]
+            for x in floorx:
+                for y in floory:
+                    if random.choice([True, False]):
+                        matrix[y,x] = 2
+                    else:
+                        matrix[y,x] = 3
 
         return matrix
